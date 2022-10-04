48f4a9289a0f8b0f10fcc5808a14df201e840926
emgreye <114972609+emgreye@users.noreply.github.com>
2022-10-04 12:55:17 +0800

    Update trove.c

--- trove.c
+++ trove.c
@@ -3,11 +3,6 @@
 //  Student2:   22884473   GAUDIN   LÉO   CONTRIBUTION
 
 #include <trove.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h> 
-#include <stdbool.h>
-#include <string.h>
 
 int main(int argcount, char *argvalue[]) {
     
