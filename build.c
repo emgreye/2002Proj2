a2f0c42e7ceda14c13fbaaf8596e5680a225c468
Léo <leogaudinoff@gmail.com>
2022-10-09 09:01:40 +0100

    Handle file build error

--- build.c
+++ build.c
@@ -1,7 +1,13 @@
 #include "trove.h"
+#include <sys/stat.h>
 
 void buildtrove(char filename[], char contents[]) {
     FILE *fp;
-    fp = fopen(filename, "rw");
+    fp = fopen(filename, "w");
+    if (fp == NULL) {
+        printf("Could not build file!");
+        exit(EXIT_FAILURE);
+    }
+    fputs(contents, fp);
     fclose(fp);
 }
