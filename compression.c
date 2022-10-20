f75df843dfd8f7230dd98f6a0361164d951411f9
emgreye <114972609+emgreye@users.noreply.github.com>
2022-10-20 19:42:04 +0800

    Added possible compression method

--- compression.c
+++ compression.c
@@ -5,6 +5,10 @@ void compresstrove(const char *path) {
   // char command[1024];
   // sprintf(command, "gzip -9c %s > %s", path, path);
   // execl("/bin/sh", "/bin/sh", "-c", command, NULL);
+  char command[6 + strlen(path)];
+  strcpy(command, "gzip ");
+  strcpy(command+5, path);
+  system(command);
 }
 
 void readcompressed(const char *path){
