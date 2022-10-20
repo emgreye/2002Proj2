bdd47399b4adc3f59831573dde5b557f480b02c3
emgreye <114972609+emgreye@users.noreply.github.com>
2022-10-20 17:25:30 +0800

    Added declarations for functions

--- compression.c
+++ compression.c
@@ -1,4 +1,10 @@
 #include "trove.h"
 #include <process.h>
 
-execl("/usr/bin/gzip","gzip", NULL);
+void compresstrove(const char *path) {
+  //execl("/usr/bin/gzip","gzip", NULL);
+}
+
+void readcompressed(const char *path){
+  //code goes here
+}
