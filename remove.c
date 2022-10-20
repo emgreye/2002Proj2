806e115cd2ba424a2bd96f19f068c8c94d27e0f7
emgreye <114972609+emgreye@users.noreply.github.com>
2022-10-20 12:25:14 +0800

    added basic body to remove

--- remove.c
+++ remove.c
@@ -1,1 +1,14 @@
 #include "trove.h"
+#include <sys/stat.h>
+
+void removetrove(char filelist[]) {
+    FILE *fptr;
+    fptr = fopen(filename, "w");
+    if (fptr == NULL) {
+        printf("Unable to update trove-file.\n");
+        exit(EXIT_FAILURE);
+    }
+  
+  
+    fclose(fptr);
+}
